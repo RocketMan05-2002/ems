392fce9bf81ac8b6d7a255454046c16205831089
Rocketman <abhinayshambharkar1@gmail.com>
2025-07-08 23:50:42 +0530

    create rewards section for employee bonuses

--- src/utils/sidebarIcons.js
+++ src/utils/sidebarIcons.js
@@ -7,41 +7,41 @@ import { IoSettingsSharp } from "react-icons/io5"; //settings
 import { FaMoneyBillTrendUp } from "react-icons/fa6"; //rewards
 
 const sidebarIcons = [
-    {
-        id: 1,
-        title: "Dashboard",
-        icon: <MdDashboardCustomize size={25} />
-    },
-    {
-        id: 2,
-        title: "My Profile",
-        icon: <FaUserTie size={25}/>
-    },
-    {
-        id: 3,
-        title: "Referrals",
-        icon: <VscReferences size={25}/>
-    },
-    {
-        id: 4,
-        title: "Leave",
-        icon: <SlCalender size={25}/>
-    },
-    {
-        id: 5,
-        title: "Salary",
-        icon: <GiReceiveMoney size={25}/>
-    },
-    {
-        id: 6,
-        title: "Settings",
-        icon: <IoSettingsSharp size={25}/>
-    },
-    {
-        id: 7,
-        title: "Rewards",
-        icon: <FaMoneyBillTrendUp size={25}/>
-    }
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: <MdDashboardCustomize size={25} />,
+  },
+  {
+    id: 2,
+    title: "My Profile",
+    icon: <FaUserTie size={25} />,
+  },
+  {
+    id: 3,
+    title: "Referrals",
+    icon: <VscReferences size={25} />,
+  },
+  {
+    id: 4,
+    title: "Leave",
+    icon: <SlCalender size={25} />,
+  },
+  {
+    id: 5,
+    title: "Salary",
+    icon: <GiReceiveMoney size={25} />,
+  },
+  {
+    id: 6,
+    title: "Settings",
+    icon: <IoSettingsSharp size={25} />,
+  },
+  {
+    id: 7,
+    title: "Rewards",
+    icon: <FaMoneyBillTrendUp size={25} />,
+  },
 ];
 
 export default sidebarIcons;
